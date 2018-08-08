b055c9e8c1a9887fd28550467efb1e0ba633290f
Rui Cheng <ktei2008@gmail.com>
2018-08-08 22:08:10 +1000

    favicon s3 URI

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 npm run clean &&\
 npm run build &&\
-aws s3 sync ./build s3://lms.cokecoder.net --storage-class REDUCED_REDUNDANCY
+aws s3 sync ./build s3://lms.cokecoder.net --storage-class REDUCED_REDUNDANCY --delete
